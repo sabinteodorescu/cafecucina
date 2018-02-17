a063dc6327eeeb47fadf5b033ade54b9057000c4
Teodorescu Sabin <36563618+saby4k@users.noreply.github.com>
2018-02-17 13:01:05 +0200

    Add code

---
+++ hosts.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html>
+	<head>
+		<title>Hosts</title>
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+		<link rel="stylesheet" href="assets/css/reset.css">
+		<link rel="stylesheet" href="assets/css/style.css">
+	</head>
+	<body>
+		<header id="header" class="hosts">
+			<div class="wrapper cf">
+				<a href="index.php" title="Cafe Cucina" id="logo"><b>Cafe</b> cucina</a>
+				<nav id="menu">
+					<ul>
+						<li><a class="active" href="#">The hosts</a></li>
+						<li><a href="#">Menu</a></li>
+						<li><a href="#">Reservations</a></li>
+						<li><a href="#">News</a></li>
+						<li><a href="#">Locations</a></li>
+						<li><a href="#">Gift voucher</a></li>
+					</ul>
+				</nav>
+
+				<h2>The hosts</h2>
+			</div>
+		</header>
+
+		<section class="staff" class="cf">
+			<div class="wrapper">
+				<div class="cf">	
+					<div class="johnsmith">
+						<h3>John Smith</h3>
+						<h6>Manager and Co-Owner</h6>
+
+						<p>Cucina, the Italian word for kitchen, has been serving authentically delicious Italian cuisine north of the river Liffey for over 13 years. It is a cosy restaurant located on Parnell Street which is now vastly improved from previous years and is well worth a visit.</p>
+						<p>They cannot be beaten in quality or price and have firmly established themselves as the No.1.</p>
+					</div>
+					<div class="paddymurphy">
+						<h3>Paddy Murphy</h3>
+						<h6>Head Chef</h6>
+
+						<p>Cucina, the Italian word for kitchen, has been serving authentically delicious Italian cuisine. 
+						It is a cosy restaurant located on Parnell Street which is now vastly improved from previous years and is well worth a visit.</p>
+						<p>They cannot be beaten in quality or price and have firmly established themselves as the No.1 eatery on Parnell Street serving delicious pizzas, pastas, seafood, chicken & steak.</p>
+					</div>
+				</div>
+					<div class="foodimg" class="cf">
+						<img src="assets/img/img-24.jpg" alt="">
+						<img src="assets/img/img-25.jpg" alt="">
+					</div>
+			</div>
+		</section>
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,137 @@
+<!doctype html>
+<html>
+	<head>
+		<title>Home</title>
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+		<link rel="stylesheet" href="assets/css/reset.css">
+		<link rel="stylesheet" href="assets/css/style.css">
+	</head>
+	<body>
+		<header id="header" style="background-image:url('assets/img/img-1.jpg');">
+			<div class="wrapper cf">
+				<a href="index.php" title="Cafe Cucina" id="logo"><b>Cafe</b> cucina</a>
+				<nav id="menu">
+					<ul>
+						<li><a href="hosts.php">The hosts</a></li>
+						<li><a href="#">Menu</a></li>
+						<li><a href="#">Reservations</a></li>
+						<li><a href="#">News</a></li>
+						<li><a href="#">Locations</a></li>
+						<li><a href="#">Gift voucher</a></li>
+					</ul>
+				</nav>
+			</div>
+			<h1>Hello dublin! Here we are!</h1>
+		</header>
+
+		<section id="about">
+			<div class="wrapper cf">
+				<div class="description">
+					<h2>About us</h2>
+					<p>Cucina, the Italian word for kitchen, has been serving authentically delicious Italian cuisine north of the river Liffey for over 13 years.</p>
+					<p>It is a cosy restaurant located on Parnell Street which is now vastly improved from previous years and is well worth a visit.</p>
+				</div>
+				<div class="photos">
+					<div class="leftphotos">
+						<img src="assets/img/img-2.jpg" alt="img-2">
+						<img src="assets/img/img-3.jpg" alt="img-3">
+					</div>
+					<div class="rightphotos">
+						<img src="assets/img/img-4.jpg" alt="img-4">
+					</div>
+				</div>
+			</div>
+		</section>
+		
+		<section id="reservation">
+			<div class="wrapper">
+				<h2>Be our guest</h2>
+				<h4>We cook not only taste, but also memories...</h4>
+				<a href="#" class="button">Make a reservation</a>
+			</div>
+		</section>
+
+		<section id="menus">
+			<div class="wrapper">
+				<img src="assets/img/img-6.jpg" alt="img-6">
+				<img src="assets/img/img-7.jpg" alt="img-7">
+				<img src="assets/img/img-8.jpg" alt="img-8">
+
+				<div class="cf">
+					<p>Cucina, the Italian word for kitchen, has been serving authentically delicious Italian cuisine north of the river Liffey for over 13 years.</p>
+					<p>Cucina, the Italian word for kitchen, has been serving authentically delicious Italian cuisine. 
+					It is a cosy restaurant located on Parnell Street which is now vastly improved from previous years and is well worth a visit.</p>
+				</div>
+				
+				<a href="#" class="button">View menus</a>
+			</div>
+		</section>
+
+		<section id="hosts">
+			<div class="wrapper">
+				<h2>The hosts</h2>
+			</div>
+		</section>
+
+		<section class="staff" class="cf">
+			<div class="wrapper">
+				<div class="cf">	
+					<div class="johnsmith">
+						<h3>John Smith</h3>
+						<h6>Manager and Co-Owner</h6>
+
+						<p>Cucina, the Italian word for kitchen, has been serving authentically delicious Italian cuisine north of the river Liffey for over 13 years. It is a cosy restaurant located on Parnell Street which is now vastly improved from previous years and is well worth a visit.</p>
+						<p>They cannot be beaten in quality or price and have firmly established themselves as the No.1.</p>
+					</div>
+					<div class="paddymurphy">
+						<h3>Paddy Murphy</h3>
+						<h6>Head Chef</h6>
+
+						<p>Cucina, the Italian word for kitchen, has been serving authentically delicious Italian cuisine. 
+						It is a cosy restaurant located on Parnell Street which is now vastly improved from previous years and is well worth a visit.</p>
+						<p>They cannot be beaten in quality or price and have firmly established themselves as the No.1 eatery on Parnell Street serving delicious pizzas, pastas, seafood, chicken & steak.</p>
+					</div>
+				</div>
+					<div class="foodimg" class="cf">
+						<img src="assets/img/img-10.jpg" alt="">
+						<img src="assets/img/img-11.jpg" alt="">
+						<img src="assets/img/img-12.jpg" alt="">
+						<img src="assets/img/img-13.jpg" alt="">
+						<img src="assets/img/img-14.jpg" alt="">
+						<img src="assets/img/img-15.jpg" alt="">
+						<img src="assets/img/img-16.jpg" alt="">
+						<img src="assets/img/img-17.jpg" alt="">
+					</div>
+			</div>
+		</section>
+
+		<footer>
+			<div class="wrapper">
+				<div id="stuff" class="cf">
+					<div id="location">
+						<h5>Location</h5>
+
+						<p>Unit 9A, Kings Court, Parnell Street, City Centre</p>
+						<p>North</p>
+						<p>Dublin 1</p>
+					</div>
+					<div id="hours">
+						<h5>Opening hours</h5>
+
+						<ol>
+							<ul>Monday: 12:00 - 22:00</ul>
+							<ul>Tuesday: 12:00 - 22:00</ul>
+							<ul>Wednesday: 12:00 - 22:00</ul>
+							<ul>Thursday: 12:00 - 22:00</ul>
+							<ul>Friday: 12:00 - 22:30</ul>
+							<ul>Saturday: 12:00 - 22:30</ul>
+							<ul>Sunday: 12:00 - 22:00</ul>
+						</ol>
+					</div>
+				</div>
+			</div>
+		</footer>
+
+		<script src="assets/js/scripts.js"></script>
+	</body>
+</html>
